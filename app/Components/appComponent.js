5284d816a265a655804791133354f8fff045cf3c
IbrahimSam96 <Ibrahimsam96@gmail.com>
2023-09-14 19:03:12 -0400

    Added Sound + Loading Screen

--- app/Components/appComponent.js
+++ app/Components/appComponent.js
@@ -1,7 +1,7 @@
 'use client'
 
 import { Canvas } from "@react-three/fiber";
-import { Suspense } from "react";
+import { Suspense, useEffect, useState } from "react";
 import { CameraControls, Loader, useProgress } from "@react-three/drei";
 import { Experience } from "./Experience";
 import { Leva } from "leva";
@@ -10,11 +10,12 @@ import { Bloom, EffectComposer } from "@react-three/postprocessing";
 
 export default function AppComponent() {
 
-    const { progress } = useProgress();
+    const [started, setStarted] = useState(false);
 
     return (
         <>
             <Leva hidden />
+
             <Canvas className={`col-start-1 col-span-8 row-start-1`} shadows camera={{ position: [13, 7, 10], fov: 60 }}  >
                 <EffectComposer>
                     <Bloom
@@ -25,11 +26,38 @@ export default function AppComponent() {
                     />
                 </EffectComposer>
                 <Suspense>
-                    {/* <CameraControls /> */}
-                    <Experience />
+                    <Experience started={started} />
                 </Suspense>
             </Canvas>
-            <Loader />
+
+            <LoadingScreeen started={started} setStarted={setStarted} />
+
+            {/* <Loader /> */}
         </>
+    )
+}
+
+
+export const LoadingScreeen = ({ started, setStarted }) => {
+
+    const { progress } = useProgress();
+
+    return (
+
+        <div className={`menu col-start-1 col-span-8 row-start-1 transition-all duration-1000 ease-in-out ${started ? 'hidden opacity-0 pointer-events-none' : 'grid'} `}>
+
+            <button onClick={() => {
+                if (progress > 100) {
+                    // Do Nothing
+                }
+                else {
+                    setStarted(true)
+                }
+            }} disabled={progress > 100}
+                className={`bg-[rgb(215,186,223,60%)] px-10 py-6 font-extrabold text-white rounded-[4px] border-none self-center justify-self-center transition-all duration-500 hover:bg-[#fffffff5] hover:text-black hover:cursor-pointer`}>
+                Start
+            </button>
+
+        </div>
     )
 }
